cd837e34d3d62f941201c528782ac81bbf4fe67a
yutaka <purewater0901@gmail.com>
2019-11-01 13:11:03 +0900

    initial commit

---
+++ src/convex_speed_optimizer.cpp
@@ -0,0 +1,187 @@
+#include "speed_planner/convex_speed_optimizer.h"
+
+ConvexSpeedOptimizer::ConvexSpeedOptimizer
+                       (const double previewDistance, 
+                        const double ds, 
+                        const double mass,
+                        const double mu,
+                        std::array<double, 5>& weight)
+: previewDistance_(previewDistance), 
+  ds_(ds), 
+  mass_(mass), 
+  mu_(mu), 
+  gravity_(9.83), 
+  epsilon_(1e-6),
+  weight_(weight)
+{
+    if(ds_<1e-10)
+        ds_ = 0.1;
+}
+
+bool ConvexSpeedOptimizer::calcOptimizedSpeed(ReferenceTrajectory& trajectory,
+                                              std::vector<double>& resultSpeed, 
+                                              const std::vector<double>& Vr,
+                                              const std::vector<double>& Vd,
+                                              const std::vector<double>& Arlon,
+                                              const std::vector<double>& Arlat,
+                                              const std::vector<double>& Aclon,
+                                              const std::vector<double>& Aclat,
+                                              const double V0,
+                                              const double a0,
+                                              const double collisionTime,
+                                              const double collisionDistance,
+                                              const double safeTime)
+{
+    int N = trajectory.size();
+    int variableSize = 8*N-1+2*(N-1);
+
+    try
+    {
+        /* Create Environment */
+        GRBEnv env = GRBEnv();
+        GRBModel model = GRBModel(env);
+        //model.set(GRB_IntParam_OutputFlag, 0);
+
+        //Create Variables
+        std::vector<GRBVar> variables;
+        variables.resize(variableSize);
+        variables[0] = model.addVar(V0*V0, V0*V0, 0.0, GRB_CONTINUOUS, "b"+std::to_string(0));
+        for(int i=1; i<N; ++i)
+            variables[i] = model.addVar(0.0, Vr[i]*Vr[i], 0.0, GRB_CONTINUOUS, "b"+std::to_string(i));
+
+        variables[N] = model.addVar(a0, a0, a0, GRB_CONTINUOUS, "a0");
+        for(int i=N+1; i<2*N-1; ++i)
+            variables[i] = model.addVar(-Arlon[i-N], Arlon[i-N], 0.0, GRB_CONTINUOUS, "a"+std::to_string(i-N));
+        variables[2*N-1] = model.addVar(0.0, 0.0, 0.0, GRB_CONTINUOUS, "a"+std::to_string(N-1));
+
+        for(int i=2*N; i<4*N; ++i)
+            variables[i] = model.addVar(0.0, GRB_INFINITY, 0.0, GRB_CONTINUOUS, "slack"+std::to_string(i-2*N));
+
+        for(int i=4*N; i<6*N; ++i)
+            variables[i] = model.addVar(-GRB_INFINITY, GRB_INFINITY, 0.0, GRB_CONTINUOUS, "u"+std::to_string(i-4*N));
+
+        for(int i=6*N; i<7*N; ++i)
+            variables[i] = model.addVar(epsilon_, GRB_INFINITY, 0.0, GRB_CONTINUOUS, "cv"+std::to_string(i-6*N));
+
+        for(int i=7*N; i<8*N-1; ++i)
+            variables[i] = model.addVar(-GRB_INFINITY, GRB_INFINITY, 0.0, GRB_CONTINUOUS, "d"+std::to_string(i-7*N));
+
+        for(int i=8*N-1; i<8*N-1+(N-1)*2; ++i)
+            variables[i] = model.addVar(0.0, GRB_INFINITY, 0.0, GRB_CONTINUOUS, "spe"+std::to_string(i-(8*N-1)));
+
+        //set objective
+        GRBLinExpr  Jt = 0.0;
+        GRBQuadExpr Js = 0.0;
+        GRBQuadExpr Jv = 0.0;
+        GRBQuadExpr JLonSlack = 0.0;
+        GRBQuadExpr JLatSlack = 0.0;
+        for(size_t i=0; i<N-1; ++i)
+        {
+            Jt += 2*ds_*variables[i+7*N];
+            Js += (variables[i+N+1]-variables[i+N])*(variables[i+N+1]-variables[i+N])/ds_;
+            Jv += (variables[i] - Vd[i]*Vd[i])*(variables[i]-Vd[i]*Vd[i])*ds_;
+            JLonSlack += variables[i+2*N] * variables[i+2*N];
+            JLatSlack += variables[i+3*N] * variables[i+3*N];
+        }
+        Jv += (variables[N-1] - Vd[N-1]*Vd[N-1])*(variables[N-1] - Vd[N-1]*Vd[N-1])*ds_;
+        JLonSlack += variables[3*N-1] * variables[3*N-1];
+        JLatSlack += variables[4*N-1] * variables[4*N-1];
+        std::cout << "weight 1: " << weight_[1] << std::endl;
+        std::cout << "weight 2: " << weight_[2] << std::endl;
+        std::cout << "weight 3: " << weight_[3] << std::endl;
+        std::cout << "weight 4: " << weight_[4] << std::endl;
+
+        //model.setObjective(weight_[0]*Jt+weight_[1]*Js+weight_[2]*Jv+weight_[3]*JLonSlack+weight_[4]*JLatSlack, GRB_MINIMIZE);
+        model.setObjective(weight_[1]*Js+weight_[2]*Jv+weight_[3]*JLonSlack+weight_[4]*JLatSlack, GRB_MINIMIZE);
+
+        /* constraint1 speed and acceleration constraints */
+        for(int i=0; i<N-1; ++i)
+            model.addConstr((variables[i+1]-variables[i])/ds_ - 2*variables[i+N] == 0, "c"+std::to_string(i));
+
+        // constraints2 longitudinal and lateral acceleration
+        // longitudinal comfort constraints
+        for(int i=0; i<N; ++i)
+        {
+            model.addConstr(Aclon[i]+variables[i+2*N] >= variables[i+N], "absconstr"+std::to_string(i));
+            model.addConstr(-(Aclon[i]+variables[i+2*N]) <= variables[i+N], "absconstr"+std::to_string(i+N));
+        }
+
+        // lateral comfort constraints
+        for(int i=0; i<N; ++i)
+        {
+            model.addConstr(mass_*(Aclat[i]+variables[i+3*N]) >= variables[i+5*N], "c"+std::to_string(i+N));
+            model.addConstr(-mass_*(Aclat[i]+variables[i+3*N]) <= variables[i+5*N], "c"+std::to_string(i+2*N));
+        }
+
+        // constraints3 friction circle
+        for(int i=0; i<N; ++i)
+        {
+            model.addQConstr(variables[i+4*N]*variables[i+4*N]+variables[i+5*N]*variables[i+5*N]<=(mu_*mass_*gravity_)*(mu_*mass_*gravity_),
+                            "qc"+std::to_string(i));
+            model.addConstr(variables[i+4*N]<=mass_*Arlon[i], "c"+std::to_string(i+3*N));
+        }
+
+        // constraint4  dynamics constraints
+        for(int i=2; i<N-1; ++i)
+        {
+            double rx1 = (trajectory.x_[i+1]-trajectory.x_[i])/ds_;
+            double rx2 = (trajectory.x_[i-2]-trajectory.x_[i-1]-trajectory.x_[i]+trajectory.x_[i+1])/(2*ds_*ds_);
+            model.addConstr(variables[i+4*N]*cos(trajectory.yaw_[i]) - variables[i+5*N]*sin(trajectory.yaw_[i]) ==
+                           (rx2 * variables[i] + rx1 * variables[i+N]), "c"+std::to_string(i+4*N));
+
+            double ry1 = (trajectory.y_[i+1]-trajectory.y_[i])/ds_;
+            double ry2 = (trajectory.y_[i-2]-trajectory.y_[i-1]-trajectory.y_[i]+trajectory.y_[i+1])/(2*ds_*ds_);
+            model.addConstr(variables[i+4*N]*sin(trajectory.yaw_[i]) + variables[i+5*N]*cos(trajectory.yaw_[i]) ==
+                           (ry2 * variables[i] + ry1 * variables[i+N]), "c"+std::to_string(i+5*N-3));
+        }
+
+        /* constraints5 time window constraints */
+        /*
+        for(int i=0; i<N-1; ++i)
+        {
+            model.addConstr(variables[i+(8*N-1)]==variables[i+6*N+1]+variables[i+6*N]-variables[i+7*N]+epsilon_, "se1"+std::to_string(i));
+            model.addConstr(variables[i+(9*N-2)]==variables[i+6*N+1]+variables[i+6*N]+variables[i+7*N]+epsilon_, "se2"+std::to_string(i));
+            model.addQConstr(4+variables[i+(8*N-1)]*variables[i+(8*N-1)]<=variables[i+(9*N-2)]*variables[i+(9*N-2)], "qc"+std::to_string(i+2*N));
+        }
+
+        for(int i=0; i<N; ++i)
+            model.addQConstr(variables[i]>=variables[i+6*N]*variables[i+6*N], "qc"+std::to_string(i+3*N));
+            */
+
+
+        /*
+        int collisionSize = int(collisionDistance/ds_);
+        GRBLinExpr timeWindowLin=0.0;
+        for(int i=0; i<collisionSize; ++i)
+            timeWindowLin += 2*ds_*variables[i+7*N];
+        //model.addConstr(timeWindowLin-collisionTime<=safeTime, "timeWindow1");
+        //model.addConstr(timeWindowLin-collisionTime>=0.0, "timeWindow2");
+        model.addConstr(timeWindowLin<=collisionTime, "timeWindow1");
+        model.addConstr(timeWindowLin>=0.0, "timeWindow2");
+        */
+
+        //Optimization step
+        model.optimize();
+
+        if(resultSpeed.size() != N)
+            resultSpeed.resize(N);
+
+        for(int i=0; i<N; ++i)
+            resultSpeed[i] = std::sqrt(variables[i].get(GRB_DoubleAttr_X));
+    }
+    catch(GRBException e)
+    {
+        std::cout << "Error code = " << e.getErrorCode() << std::endl;
+        std::cout << e.getMessage() << std::endl;
+
+        return false;
+    }
+    catch(...)
+    {
+        std::cout << "Exception during optimization" << std::endl;
+        
+        return false;
+    }
+
+    return true;
+}
---
+++ src/speed_planner_node.cpp
@@ -0,0 +1,244 @@
+#include "speed_planner/speed_planner_node.h"
+
+int main(int argc, char** argv)
+{
+
+    ros::init(argc, argv, "speed_planner");
+    SpeedPlannerNode node;
+    ros::spin();
+
+    return 0;
+}
+
+SpeedPlannerNode::SpeedPlannerNode() : nh_(), private_nh_("~"), isInitialize_(false), timer_callback_dt_(0.1)
+{
+    double mass;
+    double mu;
+    double ds;
+    double previewDistance;
+    std::array<double, 5> weight;
+    private_nh_.param<double>("mass", mass, 1500.0);
+    private_nh_.param<double>("mu", mu, 0.8);
+    private_nh_.param<double>("ds", ds, 0.1);
+    private_nh_.param<double>("preview_distance", previewDistance, 20);
+    private_nh_.param<double>("curvature_weight", curvatureWeight_, 20);
+    private_nh_.param<double>("time_weight", weight[0], 0.0);
+    private_nh_.param<double>("smooth_weight", weight[1], 15.0);
+    private_nh_.param<double>("velocity_weight", weight[2], 0.001);
+    private_nh_.param<double>("longitudinal_slack_weight", weight[3], 1.0);
+    private_nh_.param<double>("lateral_slack_weight", weight[4], 10.0);
+
+    speedOptimizer_.reset(new ConvexSpeedOptimizer(previewDistance, ds, mass, mu, weight));
+
+    optimized_waypoints_pub_ = nh_.advertise<autoware_msgs::Lane>("final_waypoints", 1, true);
+    optimized_waypoints_debug_ = nh_.advertise<geometry_msgs::Twist>("optimized_speed_debug", 1, true);
+    final_waypoints_sub_ = nh_.subscribe("safety_waypoints", 1, &SpeedPlannerNode::waypointsCallback, this);
+    current_pose_sub_ = nh_.subscribe("/current_pose", 1, &SpeedPlannerNode::currentPoseCallback, this);
+    current_status_sub_ = nh_.subscribe("/vehicle_status", 1, &SpeedPlannerNode::currentStatusCallback, this);
+    current_velocity_sub_ = nh_.subscribe("/current_velocity", 1, &SpeedPlannerNode::currentVelocityCallback, this);
+    nav_goal_sub_ = nh_.subscribe("/move_base_simple/goal", 1, &SpeedPlannerNode::navGoalCallback, this);
+    objects_sub_ = nh_.subscribe("/detection/fake_perception/objects", 1, &SpeedPlannerNode::objectsCallback, this);
+    timer_ = nh_.createTimer(ros::Duration(timer_callback_dt_), &SpeedPlannerNode::timerCallback, this);
+}
+
+void SpeedPlannerNode::waypointsCallback(const autoware_msgs::Lane& msg)
+{
+  in_lane_ptr_.reset(new autoware_msgs::Lane(msg));
+}
+
+void SpeedPlannerNode::currentPoseCallback(const geometry_msgs::PoseStamped & msg)
+{
+  in_pose_ptr_.reset(new geometry_msgs::PoseStamped(msg));
+}
+
+void SpeedPlannerNode::currentVelocityCallback(const geometry_msgs::TwistStamped& msg)
+{
+  
+  in_twist_ptr_.reset(new geometry_msgs::TwistStamped(msg));
+}
+
+void SpeedPlannerNode::currentStatusCallback(const autoware_msgs::VehicleStatus& msg)
+{
+  in_status_ptr_.reset(new autoware_msgs::VehicleStatus(msg));
+}
+
+void SpeedPlannerNode::navGoalCallback(const geometry_msgs::PoseStamped& msg)
+{
+  std::string target_frame = "map";
+  std::string source_frame = "world";
+  geometry_msgs::TransformStamped transform;
+  try
+  {
+    transform = tf2_buffer_ptr_->lookupTransform(target_frame, source_frame, ros::Time(0));
+    geometry_msgs::PoseStamped msg_in_map;
+    tf2::doTransform(msg, msg_in_map, transform);
+    in_nav_goal_ptr_.reset(new geometry_msgs::PoseStamped(msg_in_map));
+  }
+  catch (tf2::TransformException &ex)
+  {
+    ROS_WARN("%s", ex.what());
+  }
+}
+
+void SpeedPlannerNode::objectsCallback(const autoware_msgs::DetectedObjectArray& msg)
+{
+  if(in_lane_ptr_)
+  {
+    if(msg.objects.size() == 0)
+    {
+      std::cerr << "ssize of objects is 0" << std::endl;
+      return;
+    }
+    geometry_msgs::TransformStamped objects2map_tf;
+    try
+    {
+        objects2map_tf = tf2_buffer_ptr_->lookupTransform(
+          /*target*/  in_lane_ptr_->header.frame_id, 
+          /*src*/ msg.header.frame_id,
+          ros::Time(0));
+    }
+    catch (tf2::TransformException &ex)
+    {
+        ROS_WARN("%s", ex.what());
+        return;
+    }
+    in_objects_ptr_.reset(new autoware_msgs::DetectedObjectArray(msg));
+    in_objects_ptr_->header.frame_id = in_lane_ptr_->header.frame_id;
+    for(auto& object: in_objects_ptr_->objects)
+    {
+      object.header.frame_id = in_lane_ptr_->header.frame_id;
+      geometry_msgs::PoseStamped current_object_pose;
+      current_object_pose.header = object.header;
+      current_object_pose.pose = object.pose;
+      geometry_msgs::PoseStamped transformed_pose;
+      tf2::doTransform(current_object_pose, transformed_pose, objects2map_tf);
+      object.pose = transformed_pose.pose;
+    }
+  }
+}
+
+void SpeedPlannerNode::timerCallback(const ros::TimerEvent& e)
+{
+    if(in_lane_ptr_ && in_twist_ptr_)
+    {
+        int waypointSize = in_lane_ptr_->waypoints.size();
+        std::vector<double> x(waypointSize, 0.0);
+        std::vector<double> y(waypointSize, 0.0);
+
+        for(size_t id=0; id<waypointSize; ++id)
+        {
+            x[id] = in_lane_ptr_->waypoints[id].pose.pose.position.x;
+            y[id] = in_lane_ptr_->waypoints[id].pose.pose.position.y;
+        }
+
+        //1. create trajectory
+        int previewSize = speedOptimizer_->previewDistance_/speedOptimizer_->ds_;
+        ReferenceTrajectory trajectory(x, y, speedOptimizer_->ds_, previewSize);
+
+        int smoothNum = 10;
+        for(size_t i=smoothNum; i<trajectory.curvature_.size(); ++i)
+        {
+            double tmpSum = 0.0;
+            for(int j=0; j<=smoothNum; ++j)
+                tmpSum += trajectory.curvature_[i-j];
+            trajectory.curvature_[i] = tmpSum/(smoothNum+1);
+        }
+
+        //2. Create Speed Constraints and Acceleration Constraints
+        int N = trajectory.size();
+        std::vector<double> Vr(N, 0.0);     //restricted speed array
+        std::vector<double> Vd(N, 0.0);     //desired speed array
+        std::vector<double> Arlon(N, 0.0);  //acceleration longitudinal restriction
+        std::vector<double> Arlat(N, 0.0);  //acceleration lateral restriction
+        std::vector<double> Aclon(N, 0.0);  //comfort longitudinal acceleration restriction
+        std::vector<double> Aclat(N, 0.0);  //comfort lateral acceleration restriction
+
+        trajectory.curvature_[0] = trajectory.curvature_[1];
+        for(size_t i=0; i<Vr.size(); ++i)
+        {
+            Vr[i] = 5.0;
+            Vd[i] = Vr[i]/(1+curvatureWeight_*std::fabs(trajectory.curvature_[i]));
+        }
+
+        double mu = speedOptimizer_->mu_;
+        for(size_t i=0; i<N; ++i)
+        {
+            Arlon[i] = 0.5*mu*9.83;
+            Arlat[i] = 0.5*mu*9.83;
+            Aclon[i] = 0.4*mu*9.83;
+            Aclat[i] = 0.4*mu*9.83;
+        }
+
+        //3. initial speed and initial acceleration
+        double v0 = in_twist_ptr_->twist.linear.x;
+        std::cout << "Current Velocity: " << v0 << std::endl;
+        double a0 = 0.0;
+        if(!isInitialize_)
+        {
+          isInitialize_ = true;
+          previousVelocity_ = v0;
+        }
+        else
+        {
+          a0 = (v0-previousVelocity_)/timer_callback_dt_;
+          previousVelocity_ = v0;
+        }
+
+        //4. dyanmic 
+        double collisionTime = 0.0;
+        double collisionDistance = 0.0;
+        double safeTime = 0.0;
+
+        //Output the information
+        std::cout << "==================== Size: " << N  << "======================"<< std::endl;
+
+        //////////////////////////////////////////////////////////////////////////////////////////
+        ////////////////////////////////Calculate Optimized Speed////////////////////////////////
+        std::vector<double> result(N, 0.0);
+        if(!speedOptimizer_->calcOptimizedSpeed(trajectory, result, Vr, Vd, Arlon, Arlat, Aclon, Aclat, v0, a0, collisionTime, collisionDistance, safeTime))
+            return;
+
+        // Show result
+        /*
+        for(int i=0; i<result.size(); ++i)
+          std::cout << speedOptimizer_->ds_*i << ": " << result[i] << std::endl;
+          */
+
+        //5. set result
+        autoware_msgs::Lane speedOptimizedLane;
+        speedOptimizedLane.lane_id = in_lane_ptr_->lane_id;
+        speedOptimizedLane.lane_index = in_lane_ptr_->lane_index;
+        speedOptimizedLane.is_blocked = in_lane_ptr_->is_blocked;
+        speedOptimizedLane.increment = in_lane_ptr_->increment;
+        speedOptimizedLane.header = in_lane_ptr_->header;
+        speedOptimizedLane.cost = in_lane_ptr_->cost;
+        speedOptimizedLane.closest_object_distance = in_lane_ptr_->closest_object_distance;
+        speedOptimizedLane.closest_object_velocity = in_lane_ptr_->closest_object_velocity;
+        speedOptimizedLane.waypoints.reserve(result.size());
+
+        int scale = 1.0/speedOptimizer_->ds_;
+        for(int i=10; i<N; i++)
+        {
+            autoware_msgs::Waypoint waypoint;
+            waypoint.pose.pose.position.x = trajectory.x_[i];
+            waypoint.pose.pose.position.y = trajectory.y_[i];
+            waypoint.pose.pose.position.z = in_lane_ptr_->waypoints[0].pose.pose.position.z;
+            waypoint.pose.pose.orientation = tf::createQuaternionMsgFromYaw(trajectory.yaw_[i]);
+            waypoint.pose.header = in_lane_ptr_->header;
+            waypoint.twist.header=in_lane_ptr_->header;
+            if(result[i]+0.01>5.0)
+              waypoint.twist.twist.linear.x = 4.9;
+            else
+              waypoint.twist.twist.linear.x = (result[i]);
+            speedOptimizedLane.waypoints.push_back(waypoint);
+        }
+
+        for(int i=0; i<result.size(); ++i)
+        {
+          std::cout << Vd[i] << ": " << result[i] << std::endl;
+        }
+
+        optimized_waypoints_pub_.publish(speedOptimizedLane);
+    }
+
+}
